7ef45d2de708c2cb176be78bf2bf8818ed729bcf
ExPlo1t-php <younes_tm@outlook.com>
2023-03-30 14:55:43 +0000

    implemented tailwind using npm instead of cdn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./*.{html,js}",
+  "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
